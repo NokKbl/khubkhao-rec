c049cd6da2be5736ae9c9282f3ada8ce034a4cdc
Kornphon Noiprasert <konphon22@hotmail.com>
2018-11-01 02:16:50 +0700

    Update Backend with no database

--- khubkhaoapp/admin.py
+++ khubkhaoapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Food,Ingredient,Item,Veggie
 
-# Register your models here.
+class IngredientInline(admin.StackedInline):
+    model = Ingredient
+    extra = 1
+
+class FoodAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,          {'fields': ['food_name']}),
+        ('Details',     {'fields': ['image_location','average_price']}),
+        ('Veggie',      {'fields': ['veggie']}),
+    ]
+    inlines = [IngredientInline]
+
+# class VeggieAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,          {'fields': ['veggie_name']}),
+#     ]
+#     inlines = [IngredientInline]
+
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Veggie)
+admin.site.register(Item)
--- khubkhaoapp/views.py
+++ khubkhaoapp/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from khubkhaoapp.models import Item,Veggie,Food
 
 def IndexView(request):
+    query_one = Food.objects.all()
+    query_two = Item.objects.all()
+    query_three = Food.objects.get(id=2)
+    query_four = Veggie.objects.all()
+    query_five = Food.objects.all()
     context = {
-
+        'query_one': query_one,
+        'query_two': query_two,
+        'title': query_three,
+        'query_four': query_four,
+        'query_five': query_five,
     }
     return render(request, 'khubkhaoapp/index.html', context)
+
+
+
